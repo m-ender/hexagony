7b84f297fca6f629e03c33c857b6cdcccbc4cd89
Martin Büttner <m.buettner.d@gmail.com>
2015-09-11 00:14:26 +0200

    It's taking shape...

--- grid.rb
+++ grid.rb
@@ -4,62 +4,62 @@ class Grid
     attr_reader :size
 
     OPERATORS = {
-        #'!'  ,
+        '!' => [:output_int],
         #'"'  ,
-        #'#'  ,
+        '#' => [:choose_ip],
         #'$'  ,
-        #'%'  ,
-        #'&'  ,
+        '%' => [:mod],
+        '&' => [:mem_cpy],
         #'\'' ,
-        #'('  ,
-        #')'  ,
-        #'*'  ,
-        #'+'  ,
-        #','  ,
-        #'-'  ,
-        '.' => :nop,
-        #'/'  ,
-        #'0'  ,
-        #':'  ,
-        #';'  ,
-        #'<'  ,
-        #'='  ,
-        #'>'  ,
-        #'?'  ,
-        #'@'  ,
-        ##'A'  ,
-        # ...
-        ##'Z'  ,
-        ##'['  ,
-        #'\\'  ,
-        ##']'  ,
-        #'^'  ,
-        #'_'  ,
-        #'`'  ,
-        ##'a'  ,
-        # ...
-        ##'u'  ,
-        #'v'  ,
-        ##'w'  ,
-        # ...
-        ##'z'  ,
-        #'{'  ,
-        #'|'  ,
-        #'}'  ,
-        #'~'  ,
+        '(' => [:dec],
+        ')' => [:inc],
+        '*' => [:mul],
+        '+' => [:add],
+        ',' => [:input_char],
+        '-' => [:sub],
+        '.' => [:nop],
+        '/' => [:mirror_diag_up],
+        '0'  => [:digit, 0], '1'  => [:digit, 1], '2'  => [:digit, 2], '3'  => [:digit, 3], '4'  => [:digit, 4], '5'  => [:digit, 5], '6'  => [:digit, 6], '7'  => [:digit, 7], '8'  => [:digit, 8], '9'  => [:digit, 9],
+        ':' => [:div],
+        ';' => [:output_char],
+        '<' => [:branch_left],
+        '=' => [:mp_reverse],
+        '>' => [:branch_right],
+        '?' => [:input_int],
+        '@' => [:terminate],
+        #'A'  ,
+        # ... These will not be assigned and will set their ASCII value to the current memory cell.
+        #'Z'  ,
+        '[' => [:next_ip],
+        '\\' => [:mirror_diag_down],
+        ']' => [:prev_ip],
+        '^' => [:mp_branch],
+        '_' => [:mirror_hori],
+        #'`'  , This will not be assigned as it's used for debug annotations. 
+        #'a'  ,
+        # ... These will not be assigned and will set their ASCII value to the current memory cell.
+        #'z'  ,
+        '{' => [:mp_left],
+        '|' => [:mirror_vert],
+        '}' => [:mp_right],
+        '~' => [:neg],
     }
 
-    OPERATORS.default = [:nop]
+    OPERATORS.default_proc = proc do |hash, key|
+        [:mem_set, key]
+    end
 
     def initialize size
         @size = size
         @grid = Array.new(2*@size-1) {|j|
-            [:nop]*(2*@size-1 - (@size-1 - j).abs)
+            [[:nop]]*(2*@size-1 - (@size-1 - j).abs)
         }
     end
 
     def self.from_string(string)
-        src = string.gsub(/\s/,'')
+        src_dbg = string.gsub(/\s/,'')
+        src = src_dbg.gsub(/`/,'')
+
 
         # Find size of the grid as the smallest regular hexagon which
         # is not smaller than the source code.
@@ -70,7 +70,19 @@ class Grid
 
         grid = Grid.new(size)
 
-        grid.fill!(src.chars.map{|c| OPERATORS[c]})
+        debug = false
+        ops = []
+
+        src_dbg.each_char {|c|
+            if c == '`'
+                debug = true
+            else
+                ops << [OPERATORS[c], debug]
+                debug = false
+            end
+        }
+
+        grid.fill! ops
 
         grid
     end
@@ -106,12 +118,14 @@ class Grid
         y = -x-z
         return nil if [x.abs, y.abs, z.abs].max >= @size
 
-        return [x + @size-1, z + [i, @size-1].min]
+        i = z + @size-1
+        j = x + [i, @size-1].min
+        return [i, j]
     end
 
     def to_s
         @grid.map{|line|
-            ' '*(2*@size-1 - line.size) + line.map{|c| OPERATORS.invert[c]}*' '
+            ' '*(2*@size-1 - line.size) + line.map{|c,d| (d ? '`' : ' ') + (OPERATORS.invert[c]||c[1])}*''
         }*$/
     end
 end
